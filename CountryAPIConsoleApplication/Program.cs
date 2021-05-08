371f67066792944a3526bc5b4cc1f0cf3dfde896
jturner1405 <jturner1405jujitsu@gmail.com>
2021-05-08 20:02:07 +0100

    Altered initilization into separate methods from initilizing objects

--- CountryAPIConsoleApplication/Program.cs
+++ CountryAPIConsoleApplication/Program.cs
@@ -1,14 +1,46 @@
-﻿using System;
+﻿using CountryAPI.ConsoleApplication.Interface;
+using CountryAPIConsoleApplication.Class;
+using System;
 
 namespace CountryAPIConsoleApplication
 {
     public class Program
     {
-        static void Main(string[] args)
+        private static IValidationHelper validation;
+
+        /// <summary>
+        ///  initilize objects & start program
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            
+            validation = new ValidationHelper();
+
+            RequestISOValue();
+        }
+
+        /// <summary>
+        /// Read Console Input
+        /// </summary>
+        public static void RequestISOValue()
         {
             Console.WriteLine("Please Enter a country ISO Code?");
-            var result = Console.ReadLine();
+            var responseValue = Console.ReadLine();
+
+            Tuple<bool, string> isValid = validation.ValidString(responseValue);
+
+            if (isValid.Item1)
+            {
+                Console.WriteLine("Value is valid");
+            }
+            else
+            {
+                Console.WriteLine("Value entered is not valid. Due to " + isValid.Item2);
+            }
         }
+
+
     }
 
     
